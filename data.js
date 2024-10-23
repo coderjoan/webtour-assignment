3896c724f6fe7585bdea32a7190cd26400f86a92
Joan <87378716+coderjoan@users.noreply.github.com>
2024-10-23 16:48:07 +0800

    Upload web tour files

---
+++ data.js
@@ -0,0 +1,152 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-stone-plate-of-nus-law",
+      "name": "Stone Plate of NUS Law",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -3.098381646071072,
+        "pitch": -0.025489191106952802,
+        "fov": 1.2836595229737267
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.368070517751036,
+          "pitch": -0.009526444400304257,
+          "rotation": 0,
+          "target": "2-c-j-koh-law-library"
+        },
+        {
+          "yaw": 2.1782361212220867,
+          "pitch": 0.06349130458271013,
+          "rotation": 0,
+          "target": "1-the-summit-canteen"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.1203459595765697,
+          "pitch": 0.11790351566173385,
+          "title": "NUS Bukit Timah Campus",
+          "text": "This is where the NUS&nbsp;Faculty of Law will be located until the end of AY 24/25.&nbsp;<div>Did you also know that this is the second time the faculty is being moved to the Kent Ridge campus?</div>"
+        }
+      ]
+    },
+    {
+      "id": "1-the-summit-canteen",
+      "name": "The Summit Canteen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7835777681887741,
+          "pitch": -0.02461157524504287,
+          "rotation": 0,
+          "target": "0-stone-plate-of-nus-law"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.264962139151919,
+          "pitch": -0.06060792778051827,
+          "title": "Noodles Store at the Summit Canteen",
+          "text": "According to Google Maps, the store is very popular with Chinese students"
+        }
+      ]
+    },
+    {
+      "id": "2-c-j-koh-law-library",
+      "name": "C J Koh Law Library",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5816595519983663,
+          "pitch": 0.07065370824271255,
+          "rotation": 0,
+          "target": "0-stone-plate-of-nus-law"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.003790297814166621,
+          "pitch": 0.03449155950703542,
+          "title": "C J Koh Law Library",
+          "text": "Opening hours are 9am - 9pm on weekdays and 10am-5pm on Saturdays. Not open on Sundays and Public Holidays. Please check the NUS Libraries website."
+        }
+      ]
+    }
+  ],
+  "name": "Bukit Timah Campus",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
